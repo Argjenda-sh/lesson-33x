fea9da08c44709042469edcec9eaf9573d4ca0d4
Argjenda <argjendashabani560@gmail.com>
2025-02-25 19:00:51 +0100

    hh
    hh

--- main.js
+++ main.js
@@ -34,3 +34,37 @@ console.log(result)
 regex = /(top|best|school)/g;
 result = text.match(regex)
 console.log(result)
+
+regex = /\s/g;
+result = text.match(regex)
+console.log(result)
+
+text = "Heeeey, how are you!";
+regex = /e+/g
+result = text.match(regex)
+console.log(result)
+
+text = " so, i hope we will see each other again soon";
+regex = /so*/g;
+result = text.match(regex)
+console.log(result)
+
+text = "hello, helloo, hellooo";
+regex = /o{3}/g;
+result = text.match(regex);
+console.log(result)
+
+text = "hello, helloo, hellooo, helloooo, hellooooo,helloooooo";
+regex = /o{3,5}/g;
+result = text.match(regex);
+console.log(result)
+
+text = "Lorem8 0  ipsum dolor sit amet consectetur adipisicing elit. Neque suscipit hic exercitationem corporis explicabo mollitia iusto veritatis ab ullam fuga tempora, quam possimus, perspiciatis dolor debitis repellendus earum enim tenetur."
+
+regex = /!$/g;
+result = text.match(regex);
+console.log(result)
+
+regex = /^Lo/g;
+result = text.match(regex);
+console.log(result)
